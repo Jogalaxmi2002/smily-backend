7a4944e116202f457d5a17c9fee1ae4b315de405
mr-soumya071 <soumyaranjansahugcek@gmail.com>
2024-03-19 22:43:12 +0530

    Initial commit

--- delete.js
+++ delete.js
@@ -1,15 +1,19 @@
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
-//export async function main(event, context) {}
+
 export const main = handler(async (event, context) => {
   const params = {
-    TableName: "notes",
+    TableName: "appointment",
     // 'Key' defines the partition key and sort key of the item to be removed
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'noteId': path parameter
     Key: {
-      userId: event.requestContext.identity.cognitoIdentityId, // The id of the author
-      noteId: event.pathParameters.id, // The id of the note from the path
-    },
+      userId: event.requestContext.identity.cognitoIdentityId,
+      appointmentId: event.pathParameters.id
+    }
   };
+
   await dynamoDb.delete(params);
+
   return { status: true };
 });
--- list.js
+++ list.js
@@ -1,18 +1,21 @@
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
-//export async function main(event, context) {}
+
 export const main = handler(async (event, context) => {
   const params = {
-    TableName: "notes",
+    TableName: "appointment",
     // 'KeyConditionExpression' defines the condition for the query
-    // - 'userId = :userId': only return items with matching 'userId' partition key
-    KeyConditionExpression: "userId = :userId",
+    // - 'userId = :userId': only return items with matching 'userId'
+    //   partition key
     // 'ExpressionAttributeValues' defines the value in the condition
-    // - ':userId': defines 'userId' to be the id of the author
+    // - ':userId': defines 'userId' to be Identity Pool identity id
+    //   of the authenticated user
+    KeyConditionExpression: "userId = :userId",
     ExpressionAttributeValues: {
-      ":userId": event.requestContext.identity.cognitoIdentityId,
-    },
+      ":userId": event.requestContext.identity.cognitoIdentityId
+    }
   };
+
   const result = await dynamoDb.query(params);
 
   // Return the matching list of items in response body
